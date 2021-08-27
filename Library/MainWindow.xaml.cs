5a9ebdddcc0e00ecf4dcb2ea1738cd2160c5a0d5
Albanek203 <oleglovchuk@ukr.net>
2021-08-27 09:19:20 +0300

    Initial upload

---
+++ Library/MainWindow.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace Library {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() { InitializeComponent(); }
+    }
+}
